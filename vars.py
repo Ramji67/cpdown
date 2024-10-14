f86cfd4fbefd2e6a7c8019bbc436424f7ea37a75
Ramji67 <158837797+Ramji67@users.noreply.github.com>
2024-10-14 18:34:13 +0530

    vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 api_id = 28088290 #change_with_your_Api_id.
 api_hash = "6998f2c585fdce65ac72dfa23d02b6ec" #change_with_your_Api_hash.
-bot_token = "7657609554:AAEQsFaYWper4YpmVJqqJZkeIW7CUJ1lzS8" #change_with_your_bot_token.
+bot_token = "" #change_with_your_bot_token.
 auth_users = [5850397219] #change_with_your_user_id.
 sudo_user = 5850397219 #change_with_your_user_id.
 log_channel = -1002360642973 #change_with_your_channel_id.
