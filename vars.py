e4e9ecf63e1c1e7cc775b179cedb7aefb1daa69c
Ramji67 <158837797+Ramji67@users.noreply.github.com>
2024-10-28 20:08:25 +0530

    vars.py

--- vars.py
+++ vars.py
@@ -1,7 +1,4 @@
-api_id = 35814071 #change_with_your_Api_id.
-api_hash = "80c231f0e8704995b8259d06965n4351" #change_with_your_Api_hash.
-bot_token = "7198600769:AAHfVi7qTaMw20HbpvPrF-fnP77-sWdOthI" #change_with_your_bot_token.
-auth_users = [999741495] #change_with_your_user_id.
-sudo_user = 1713924419 #change_with_your_user_id.
-log_channel = -1002154281532 #change_with_your_channel_id.
-txt_channel = -1002154281532 #change_with_your_channel_id.
+api_id =28088290  #change_with_your_Api_id.
+api_hash = "6998f2c585fdce65ac72dfa23d02b6ec" #change_with_your_Api_hash.
+bot_token = "7770833956:AAGOhSuPdS2Kc4QPNOR6TXUWDoVAgLKmgFs" #change_with_your_bot_token.
+
